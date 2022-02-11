59e47ca56b9890c231ee67b12d5e44a34c8e5db0
screpeau <slcrepeau@wpi.edu>
2022-02-11 18:51:55 -0500

    submit 1

---
+++ code/plot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+df = pd.read_csv("cars-sample.csv")
+
+sb.set_style('darkgrid')
+splot = sb.scatterplot(data=df, 
+            x='Weight',
+            y='MPG',
+            hue='Manufacturer',
+            size='Weight',
+            style='Cylinders',
+            alpha=0.5
+            )
+
+splot.set_xticks(range(2000,6000,1000))
+splot.set_yticks(range(10,50,10))
+
+plt.legend(fontsize='x-small', framealpha=0.8)
+
+plt.show()
+
