932fb953786e17f0c92d18e132d918db4e9d23c1
ColdCode0214 <jwang21@wpi.edu>
2022-02-11 13:28:24 -0500

    Upload R

---
+++ R/A2-R-2.R
@@ -0,0 +1,14 @@
+
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("C:/Users/Lenovo/Desktop/573/A2/a2-DataVis-5ways/cars-sample.csv", header=TRUE)
+
+# d3 category 10 colors
+colors <- c("bmw"="hotpink","ford"="deepskyblue","honda"="limegreen","mercedes"="yellow","toyota"="gray")
+
+
+# Bubble plot
+ggplot(data, aes(x=Weight, y=MPG, size=Weight, color=Manufacturer, shape=Manufacturer)) + geom_point(alpha=0.5) +
+  scale_color_manual(values=colors)
+
