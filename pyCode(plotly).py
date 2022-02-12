f86730a0aacbdae9d874b88fe1d51bf2b867ec4a
mnorales <48488377+mnorales@users.noreply.github.com>
2022-02-11 19:24:29 -0500

    Update pyCode(plotly).py

--- pyCode(plotly).py
+++ pyCode(plotly).py
@@ -1,1 +1,38 @@
+# https://stackoverflow.com/questions/60962274/plotly-how-to-change-the-colorscheme-of-a-plotly-express-scatterplot
+# https://plotly.com/python/plotly-express/
+# https://plotly.com/python/figure-labels/
+# https://plotly.com/python/tick-formatting/#using-tickformat-attribute
 
+#dir(px.colors.qualitative)
+
+import pandas as pd
+import plotly.express as px
+
+file = pd.read_csv("cars-sample.csv")
+
+img = px.scatter(
+    file, 
+    x =  'Weight', 
+    y = 'MPG', 
+    color = 'Manufacturer', 
+    color_discrete_sequence = px.colors.qualitative.Bold,
+    size = 'Weight', 
+    size_max = 20
+) 
+
+img.update_layout(
+    font=dict(
+        family="Courier New, monospace",
+        size=20,
+        color="Brown"
+    ),
+    xaxis = dict(
+        tick0 = 2000,
+        dtick = 1000
+    ),
+    yaxis = dict(
+        tick0 = 10,
+        dtick = 10
+    )
+)
+img.show()
